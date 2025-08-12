4857137b708f13b372447b563a7169e1b4e3d430
CecileLAROCHE <11458@holbertonstudents.com>
2025-08-12 11:16:18 +0200

    ajput librairies dans .h et début du main.c

--- Shell.h
+++ Shell.h
@@ -1,7 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-
+/*librairie utile*/
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 
 
---
+++ main.c
@@ -0,0 +1,44 @@
+#include "Shell.h"
+
+/**
+ * main - Runs a simple shell that reads and prints user input
+ *
+ * This program continuously prompts the user for input, reads a line
+ * from standard input, and prints the command entered by the user until
+ * EOF (Ctrl+D) is encountered.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	while (1)
+	{
+
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+
+		nread = getline(&line, &len, stdin);
+
+		if (nread == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			break;
+		}
+
+		line[strcspn(line, "\n")] = '\0';
+
+		printf("Commande : %s\n", line);
+	}
+
+	free(line);
+	return (0);
+}
+
+
+
+
