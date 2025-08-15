ef90d32ae899a3ec5073d7bfa4384d877e27ec1e
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-08-15 19:53:59 +0200

    continuation du code process commande

--- process_command.c
+++ process_command.c
@@ -14,6 +14,7 @@ int process_command(char *line, char *argv[], int cmd_count, int *exit_status)
 	char *args[64];
 	int index = 0;
 	char *token = strtok(line, "\t\r\n");
+	char *cmd_path;
 
 	while (token != NULL && index < 63)
 	{
@@ -23,14 +24,29 @@ int process_command(char *line, char *argv[], int cmd_count, int *exit_status)
 
 	args[index] = NULL;
 
+	/*si ligne vide*/
 	if (args[0] == NULL)
 	{
 		return (0);
 	}
 
+	/*si exit*/
 	if (strcmp(args[0], "exit") == 0)
 	{
 		return (1);
 	}
+
+	/* recharcghe dans path*/
+	cmd_path = find_in_path(args[0]);
+	if (cmd_path != NULL)
+	{
+		execute_command(cmd_path, args, argv[0], exit_status);
+		free(cmd_path);
+	}
+	else
+	{
+		printf("%s: command not found\n", args[0]);
+	}
+	return (0);
 }
 
