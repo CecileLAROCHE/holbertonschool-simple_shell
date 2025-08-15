ce2ecd01f3657e7b53f09d52c66fa104dc2586bf
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-08-15 19:52:57 +0200

    continue le code de process-command

--- _find_path_command.c
+++ _find_path_command.c
@@ -1,3 +1,5 @@
+#include "Shell.h"
+
 
 
 cmd_path
--- execute_command.c
+++ execute_command.c
@@ -12,34 +12,31 @@ void execute_command(char *cmd_path, char **args, const char *shell_n,
 	pid_t pid;
 	int status;
 
-	pid = fprk();
+	pid = fork();
 
 	if (pid < 0) /*Le parent sait qui est l’enfant*/
 	{
-		perror("fork")
+		perror("fork");
 		*exit_stat = 1;
 		return;
 	}
 
-if (pid == 0) /* Processus enfant */
-    {
-        if (execve(cmd_path, args, environ) == -1)
-        {
-            perror(shell_n); /* ex: ./myshell: ls: No such file */
-            exit(127);       /* code d'erreur standard pour commande non trouvée */
-        }
-    }
-    else /* Processus parent */
-    {
-        waitpid(pid, &status, 0); /* Attend la fin du processus enfant */
+	if (pid == 0) /* Processus enfant */
+	{
+		if (execve(cmd_path, args, environ) == -1)
+		{
+			perror(shell_n); /* ex: ./myshell: ls: No such file */
+			exit(127);       /* code d'erreur standard pour commande non trouvée */
+		}
+	}
+	else /* Processus parent */
+	{
+		waitpid(pid, &status, 0); /* Attend la fin du processus enfant */
 
-        if (WIFEXITED(status))
-            *exit_stat = WEXITSTATUS(status);
-        else
-            *exit_stat = 1;
-    }
-}
-
-
+		if (WIFEXITED(status))
+			*exit_stat = WEXITSTATUS(status);
+		else
+			*exit_stat = 1;
+	}
 }
 
